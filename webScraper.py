4618aba886ef745848979c8454496882b178d5ce
Louix Njeru <njeruulouix@yahoo.co.uk>
2023-07-07 09:05:19 +0100

    Research

--- webScraper.py
+++ webScraper.py
@@ -27,6 +27,27 @@ def getImageUrls(url):
         
     return urls
 
-urls = getImageUrls('https://www.sneakerjagers.com/en/releases/out-now/page/100')
+def downloadImage(url,filePath):
+    if not os.path.isdir(filePath): os.makedirs(filePath)
+    
+    response = requests.get(url,stream=True)
+    fileSize = int(response.headers.get("Content-Length", 0))
+    fileName = os.path.join(filePath, url.split('/')[-1])
+    progress = tqdm(response.iter_content(1024), f"Downloading {fileName}", total=fileSize, unit="B", unit_scale=True, unit_divisor=1024)
+    
+    with open(fileName, 'wb') as f:
+        for data in progress.iterable:
+            f.write(data)
+            progress.update(len(data))
+            
+def scraper(url,filePath):
+    urls = getImageUrls(url)
+    for img in urls: downloadImage(img, filePath)
+
+'''urls = getImageUrls('https://www.sneakerjagers.com/en/releases/out-now/page/100')
 print(len(urls))
-print(urls)
+print(urls)'''
+
+for i in range(136,151):
+    print(i)
+    scraper(f'https://www.sneakerjagers.com/en/releases/out-now/page/{i}','data/trainData')
